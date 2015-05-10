2e100cf4163eba00e14f69089e739a3cd09dde0c
Kim Persson <kimpersson88@gmail.com>
2015-05-10 17:40:26 +0200

    Had to re add dep

---
+++ own_cookbooks/rubinius_dependencies/metadata.rb
@@ -0,0 +1,8 @@
+name             'rubinius_dependencies'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures rubinius_dependencies'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+depends "apt", ">= 1.8.4"
+version          '0.1.0'
